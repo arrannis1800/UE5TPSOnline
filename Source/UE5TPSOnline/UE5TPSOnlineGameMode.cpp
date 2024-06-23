bfe8050779da13ff99fb470f61ad4b5120a26cd5
Artem Yudin <arrannis1800@gmail.com>
2024-06-23 20:57:28 +0400

    Add countdown timer for matchmaking

--- Source/UE5TPSOnline/UE5TPSOnlineGameMode.cpp
+++ Source/UE5TPSOnline/UE5TPSOnlineGameMode.cpp
@@ -3,8 +3,41 @@
 #include "UE5TPSOnlineGameMode.h"
 #include "UE5TPSOnlineCharacter.h"
 #include "UObject/ConstructorHelpers.h"
+#include "EngineUtils.h"
+#include "TimerManager.h"
 
 AUE5TPSOnlineGameMode::AUE5TPSOnlineGameMode()
 {
-	
+
+    bReplicates = true;
+    bAlwaysRelevant = true;
+}
+
+void AUE5TPSOnlineGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+
+    StartMatchCountdown();
+}
+
+void AUE5TPSOnlineGameMode::StartMatchCountdown()
+{
+    GetWorldTimerManager().SetTimer(MatchCountdownTimerHandle, this, &AUE5TPSOnlineGameMode::OnMatchCountdownComplete, MatchDuration);
+}
+
+void AUE5TPSOnlineGameMode::OnMatchCountdownComplete()
+{
+    for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; ++It)
+    {
+        APlayerController* PlayerController = It->Get();
+        if (PlayerController)
+        {
+            AUE5TPSOnlineCharacter* Character = Cast<AUE5TPSOnlineCharacter>(PlayerController->GetPawn());
+            if (Character)
+            {
+                PlayerController->ChangeState(NAME_Spectating);
+                PlayerController->ClientGotoState(NAME_Spectating);
+            }
+        }
+    }
 }
--- Source/UE5TPSOnline/UE5TPSOnlineGameMode.h
+++ Source/UE5TPSOnline/UE5TPSOnlineGameMode.h
@@ -12,7 +12,19 @@ class AUE5TPSOnlineGameMode : public AGameModeBase
 	GENERATED_BODY()
 
 public:
-	AUE5TPSOnlineGameMode();
+	AUE5TPSOnlineGameMode(); 
+
+    virtual void BeginPlay() override;
+	
+	void StartMatchCountdown();
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Match Settings")
+    float MatchDuration = 300.0f;
+
+    FTimerHandle MatchCountdownTimerHandle;
+
+    void OnMatchCountdownComplete();
 };
 
 
